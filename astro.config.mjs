7a5e80329c8cea9d41c0e9c1271431d4ff1a5f01
Daen-dev <daen-dev@proton.me>
2026-09-17 01:01:13 -0300

    portfolio v1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://daen-dev.github.io',
+  base: '/portfolio-aldo-pintor-astro'
 });
